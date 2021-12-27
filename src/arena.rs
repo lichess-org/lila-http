887746e16d49ef49b7ba6a2dc6a0eb27ca89a68c
Thibault Duplessis <t@lichess.org>
2021-12-27 17:15:35 +0100

    read and serve shared data

--- src/arena.rs
+++ src/arena.rs
@@ -1,23 +1,56 @@
 use serde::{Deserialize, Serialize};
+use serde_json::Value as JsValue;
 
 #[derive(Debug, Eq, PartialEq, Deserialize, Hash, Clone)]
 pub struct ArenaId(pub String);
 
+// naming is hard
+#[derive(Debug, Clone, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct ArenaShared {
+    nb_players: u32,
+    duels: JsValue,
+    #[serde(default, skip_serializing_if = "Option::is_none")]
+    seconds_to_finish: Option<u32>,
+    #[serde(default, skip_serializing_if = "Option::is_none")]
+    seconds_to_start: Option<u32>,
+    #[serde(default, skip_serializing_if = "Option::is_none")]
+    is_started: Option<bool>,
+    #[serde(default, skip_serializing_if = "Option::is_none")]
+    is_finished: Option<bool>,
+    #[serde(default, skip_serializing_if = "Option::is_none")]
+    is_recently_finished: Option<bool>,
+    #[serde(default, skip_serializing_if = "Option::is_none")]
+    featured: Option<JsValue>,
+    #[serde(default, skip_serializing_if = "Option::is_none")]
+    podium: Option<JsValue>,
+    #[serde(default, skip_serializing_if = "Option::is_none")]
+    pairings_closed: Option<bool>,
+    #[serde(default, skip_serializing_if = "Option::is_none")]
+    stats: Option<JsValue>,
+    #[serde(default, skip_serializing_if = "Option::is_none")]
+    team_standing: Option<JsValue>,
+    #[serde(default, skip_serializing_if = "Option::is_none")]
+    duel_teams: Option<JsValue>,
+}
+
 #[derive(Debug, Clone, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct ArenaFull {
-    nb_players: u32,
+    #[serde(flatten)]
+    shared: ArenaShared,
 }
 
 #[derive(Debug, Clone, Serialize)]
 pub struct ClientData {
-    nb_players: u32,
+    #[serde(flatten)]
+    shared: ArenaShared,
 }
 
 impl ClientData {
     pub fn new(full: ArenaFull) -> ClientData {
         ClientData {
-            nb_players: full.nb_players,
+            shared: full.shared,
         }
     }
 }
