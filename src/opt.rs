a4ffe45b09c561a0586bfdddedf9ff9e1e1b366c
Niklas Fiekas <niklas.fiekas@backscattering.de>
2022-01-19 21:50:20 +0100

    remove more unused opts

--- src/opt.rs
+++ src/opt.rs
@@ -4,20 +4,13 @@ use clap::Parser;
 
 #[derive(Parser, Debug, Clone)]
 pub struct Opt {
-    /// Binding address. Note that administrative endpoints must be protected
-    /// using a reverse proxy.
+    /// Binding address.
     #[clap(long, default_value = "127.0.0.1:3000")]
     pub bind: SocketAddr,
-    /// Disable access from all origins.
+    /// Disable access from all origins, for example if a reverse proxy is
+    /// responsible for CORS.
     #[clap(long)]
     pub no_cors: bool,
-    /// Base url for the indexer.
-    #[clap(long, default_value = "http://l.org")]
-    pub lila: String,
-    /// Token of https://lichess.org/@/Lilarena to speed up indexing.
-    /// The default value is a local dev token, it won't work in production.
-    #[clap(long, default_value = "lip_2GB9ilOKexedQCI1xTkP")]
-    pub bearer: String,
     #[clap(long, default_value = "redis://localhost")]
     pub redis_url: String,
 }
