5fe363f9b70ebf71751b9533cb0f17216d01baa4
Niklas Fiekas <niklas.fiekas@backscattering.de>
2022-10-01 13:10:00 +0200

    update deps (including clap 4)

--- src/opt.rs
+++ src/opt.rs
@@ -5,18 +5,18 @@ use clap::Parser;
 #[derive(Parser, Debug)]
 pub struct Opt {
     /// Binding address.
-    #[clap(long, default_value = "127.0.0.1:3000")]
+    #[arg(long, default_value = "127.0.0.1:3000")]
     pub bind: SocketAddr,
     /// Disable access from all origins, for example if a reverse proxy is
     /// responsible for CORS.
-    #[clap(long)]
+    #[arg(long)]
     pub no_cors: bool,
-    #[clap(flatten)]
+    #[command(flatten)]
     pub redis: RedisOpt,
 }
 
 #[derive(Parser, Debug)]
 pub struct RedisOpt {
-    #[clap(long, default_value = "redis://localhost")]
+    #[arg(long, default_value = "redis://localhost")]
     pub redis_url: String,
 }
