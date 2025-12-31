4b51372a5f28682765ce0c335d059bb3f3b529ea
Lakin Wecker <lakin@wecker.ca>
2025-12-31 15:21:08 -0700

    feat: Suppport config from env

--- src/opt.rs
+++ src/opt.rs
@@ -5,11 +5,11 @@ use clap::Parser;
 #[derive(Parser, Debug)]
 pub struct Opt {
     /// Binding address.
-    #[arg(long, default_value = "127.0.0.1:9001")]
+    #[arg(long, default_value = "127.0.0.1:9001", env = "LILA_HTTP_BIND")]
     pub bind: SocketAddr,
     /// Disable access from all origins, for example if a reverse proxy is
     /// responsible for CORS.
-    #[arg(long)]
+    #[arg(long, env = "LILA_HTTP_NO_CORS")]
     pub no_cors: bool,
     #[command(flatten)]
     pub redis: RedisOpt,
@@ -17,6 +17,6 @@ pub struct Opt {
 
 #[derive(Parser, Debug)]
 pub struct RedisOpt {
-    #[arg(long, default_value = "redis://localhost")]
+    #[arg(long, default_value = "redis://localhost", env = "LILA_HTTP_REDIS_URL")]
     pub redis_url: String,
 }
